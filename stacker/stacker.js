1461db9b63aa0a51f61b817cdd7877ee566e3698
David Haynes <dhaynesxx@gmail.com>
2016-03-10 12:19:02 +1100

    first commit stacker

---
+++ stacker/stacker.js
@@ -0,0 +1,192 @@
+var column = 0;
+var direction = 1;
+var row = 11;
+var time = 600;
+var pieces = 3;
+var result = '';
+
+var resetGame = function () {
+        $('.cell').removeClass('active');
+        column = 0;
+        direction = 1;
+        row = 11;
+        time = 600;
+        pieces = 3;
+        result = '';
+        $('.start').text('Start');
+        $('.start').off('click');
+        $('.start').on('click', firstStart);
+        window.clearInterval(movingpiece);
+
+};
+
+var playThreePiece = function () {
+        if (column > 1) {
+                var z = column - direction - direction;
+                $('#pos' + row + z).removeClass('active');
+        }
+        column += direction;
+        $('#pos' + row + column).addClass('active');
+        if (column === 1) {
+          direction = 1;
+          if ($('#pos' + row + '1').hasClass('active') === true && $('#pos' + row + '2').hasClass('active') && $('#pos' + row + '3').hasClass('active')) {
+                  column = 3;
+          }
+  } else if (column === 7) {
+          direction = -1;
+          column = 5;
+        }
+        $(window).on('keypress', changeRow(event));
+};
+
+
+var playTwoPiece = function () {
+        if (column > 1) {
+                var z = column - direction;
+                $('#pos' + row + z).removeClass('active');
+        }
+        column += direction;
+        $('#pos' + row + column).addClass('active');
+        if (column === 1) {
+          direction = 1;
+          if ($('#pos' + row + '1').hasClass('active') === true && $('#pos' + row + '2').hasClass('active')) {
+                  column = 2;
+          }
+        } else if (column === 7) {
+          direction = -1;
+          column = 6;
+        }
+        $('.move').off('click').on('click', changeRow);
+};
+
+var playOnePiece = function () {
+        if (column > 0) {
+                $('#pos' + row + column).removeClass('active');
+        }
+        column += direction;
+        $('#pos' + row + column).addClass('active');
+        if (column === 1) {
+          direction = 1;
+        } else if (column === 7) {
+          direction = -1;
+        }
+        $('.move').off('click').on('click', changeRow);
+};
+
+var testMove = function () {
+        var testRow = row - 1;
+        var prevRow = row - 2;
+        for (var i = 1; i <= 7; i++) {
+                if ($('#pos' + testRow + i).hasClass('active') === true && $('#pos' + prevRow + i).hasClass('active') === false) {
+                      $('#pos' + testRow + i).removeClass('active');
+                      pieces -= 1;
+                }
+        } if (row === 29 && pieces > 0 ) {
+                result = 'win';
+                endGame();
+                return false;
+        } return true;
+};
+
+var testCurrentPieces = function () {
+        if ( pieces === 3 ) {
+                movingpiece = window.setInterval(playThreePiece, time);
+        } else if ( pieces === 2 ) {
+                movingpiece = window.setInterval(playTwoPiece, time);
+        } else if ( pieces === 1 ) {
+                movingpiece = window.setInterval(playOnePiece, time);
+        } else if ( pieces === 0 ) {
+                result = 'lose';
+                endGame();
+        }
+};
+
+var changeRow = function() {
+        if (event.keyCode === 32) {
+            console.log('space');
+            window.clearInterval(movingpiece);
+            row += 1;
+            time -= 30;
+            column = 0;
+            direction = 1;
+            if (row > 12) {
+              if (testMove() === false) {
+                return;
+              }
+            }
+            testCurrentPieces();
+        }
+};
+
+var playThree = function() {
+        movingpiece = window.setInterval(playThreePiece, time);
+};
+
+var playTwo = function() {
+        movingpiece = window.setInterval(playTwoPiece, time);
+};
+
+var playOne = function() {
+        movingpiece = window.setInterval(playOnePiece, time);
+};
+
+var firstStart = function() {
+        $('.start').off('click');
+        $('.start').text('Reset');
+        $('.start').on('click', resetGame);
+        playThree();
+};
+
+$(document).ready(function() {
+        $('.start').on('click', firstStart);
+});
+
+var endAnimate = function () {
+    if (row === 10) {
+      window.clearInterval(timerID);
+      upActive();
+      return;
+    }
+    $('.row' + row).removeClass('active');
+    row--;
+};
+
+var endGame = function() {
+        timerID = window.setInterval(endAnimate, 100);
+};
+
+var upAnimate = function () {
+    if (row === 29) {
+      window.clearInterval(timerID);
+      window.setTimeout(youGraphic, 500);
+      window.setTimeout(allActive, 1500);
+      if (result === 'win') {
+          window.setTimeout(winGraphic, 1501);
+      } else if (result === 'lose') {
+          window.setTimeout(loseGraphic, 1501);
+      }
+      return;
+    }
+    $('.row' + row).addClass('active');
+    row += 1;
+};
+
+var upActive = function() {
+        timerID = window.setInterval(upAnimate, 100);
+};
+
+var youGraphic = function () {
+      $('#pos282, #pos286, #pos272, #pos276, #pos263, #pos265, #pos254, #pos244, #pos222, #pos223, #pos224, #pos225, #pos226, #pos212, #pos216, #pos202, #pos206, #pos192, #pos196, #pos182, #pos183, #pos184, #pos185, #pos186, #pos162, #pos166, #pos152, #pos156, #pos142, #pos146, #pos132, #pos136, #pos123, #pos124, #pos125').removeClass('active');
+};
+
+var winGraphic = function () {
+      $('#pos282, #pos286, #pos272, #pos276, #pos262, #pos264, #pos266, #pos252, #pos253, #pos255, #pos256, #pos242, #pos246, #pos223, #pos224, #pos225, #pos214, #pos204, #pos194, #pos183, #pos184, #pos185, #pos162, #pos166, #pos152, #pos153, #pos156, #pos142, #pos144, #pos146, #pos132, #pos135, #pos136, #pos122, #pos126').removeClass('active');
+};
+
+var loseGraphic = function () {
+      $('#pos281, #pos271, #pos261, #pos251, #pos241, #pos242, #pos243, #pos221, #pos222, #pos223, #pos211, #pos213, #pos215, #pos216, #pos217, #pos201, #pos203, #pos205, #pos191, #pos193, #pos195, #pos196, #pos197, #pos181, #pos182, #pos183, #pos187, #pos175, #pos176, #pos177, #pos155, #pos156, #pos157, #pos145, #pos135, #pos136, #pos125, #pos115, #pos116, #pos117').removeClass('active');
+};
+
+var allActive = function () {
+      $('.cell').addClass('active');
+};
--- tictactoe2/readme.md
+++ tictactoe2/readme.md
@@ -1,30 +1,14 @@
 # Project Zero
 ## by Dave Haynes
 
+This is my first game.
 
+the features I have added include
 
-set up html so that it displays the board
-use css to style the page
+- having multiple icons
+- being able to play 1, 3 or 5 games
+- win single and multi animations
+- user can add name
 
-who goes first? /
-what piece are they, noughts or crosses? /
-what square did they select?
-make that square equal to their piece?/
-where are their previous moves?
-have they won? horizontal, vertical, diagonal?
-if yes, end game, say won, invite for new game
-if no, next player move.
-continue till win
-
-
-things to fix
-stalemate,/
-reset/
-lineup of #pieces with names
-remove icon when selected by player 1 /
-lines rather than boxes
-
-extensions
-more icons /
-play to 5 games
-play to n games
+enjoy,
+DH
